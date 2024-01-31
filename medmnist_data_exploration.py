ba44f1db560e38dd4eed8de848fa9c2621a2e791
Jack Hodgkinson <jnhodgkinson2010@hotmail.co.uk>
2024-01-31 15:18:37 +0000

    Beginning of adaptation for MedMNIST+

--- medmnist_data_exploration.py
+++ medmnist_data_exploration.py
@@ -1,4 +1,5 @@
 # Import Relevant Packages and Functions 
+
 from tqdm import tqdm
 
 import numpy as np
@@ -7,6 +8,7 @@ import matplotlib.pyplot as plt
 import torch
 import sklearn
 import os 
+import random
 from PIL import Image
 
 from sklearn.discriminant_analysis import \
@@ -22,27 +24,49 @@ import torch.utils.data as data
 from data import Dataset
 import torchvision.transforms as transforms
 
-import medmnist
-from medmnist import INFO, Evaluator, HOMEPAGE, DEFAULT_ROOT
+from medmnist.info import INFO, HOMEPAGE, DEFAULT_ROOT
+
+try:
+    from collections.abc import Sequence
+except ImportError:
+    from collections import Sequence
+
 
 #Loads each of the MedMNIST datasets and gives each of them a flag.
-class MedMNIST(Dataset):
+class MedMNIST(Sequence):
 
     flag = ...
 
-    def __init__(self,
-                 split,
-                 transform=None,
-                 target_transform=None,
-                 download=False,
-                 as_rgb=False,
-                 root=DEFAULT_ROOT):
-        ''' dataset
-        :param split: 'train', 'val' or 'test', select subset
-        :param transform: data transformation
-        :param target_transform: target transformation
+    def __init__(
+        self,
+        split,
+        transform=None,
+        target_transform=None,
+        download=False,
+        as_rgb=False,
+        root=DEFAULT_ROOT,
+        size=None,
+        mmap_mode=None
+    ):
+        ''' 
+        Args:
+
+            split (str, required): 'train', 'val' or 'test'
+            transform (callable, optional): data transformation
+            target_transform (callable, optional): target transformation
+            size (int, optional): The size of the returned images. If None, use MNIST-like 28. Default: None.
+            mmap_mode (str, optional): If not None, read image arrays from the disk directly. This is useful to set `mmap_mode='r'` to save memory usage when the dataset is large (e.g., PathMNIST-224). Default: None.
 
         '''
+
+        if (size is None) or (size == 28):
+            self.size = 28
+            self.size_flag = ""
+        else:
+            assert size in self.available_sizes
+            self.size = size
+            self.size_flag = f"_{size}"
+
 
         self.info = INFO[self.flag]
 
@@ -57,25 +81,22 @@ class MedMNIST(Dataset):
 
         if not os.path.exists(
                 os.path.join(self.root, "{}.npz".format(self.flag))):
-            raise RuntimeError('Dataset not found. ' +
-                               ' You can set `download=True` to download it')
+            raise RuntimeError('Dataset not found.' +
+                               ' You can use download=True to download it')
 
-        npz_file = np.load(os.path.join(self.root, "{}.npz".format(self.flag)))
+        npz_file = np.load(
+            os.path.join(self.root, f"{self.flag}{self.size_flag}.npz"),
+            mmap_mode=mmap_mode,
+        )
 
         self.split = split
         self.transform = transform
         self.target_transform = target_transform
         self.as_rgb = as_rgb
 
-        if self.split == 'train':
-            self.imgs = npz_file['train_images']
-            self.labels = npz_file['train_labels']
-        elif self.split == 'val':
-            self.imgs = npz_file['val_images']
-            self.labels = npz_file['val_labels']
-        elif self.split == 'test':
-            self.imgs = npz_file['test_images']
-            self.labels = npz_file['test_labels']
+        if self.split in ["train", "val", "test"]:
+            self.imgs = npz_file[f"{self.split}_images"]
+            self.labels = npz_file[f"{self.split}_labels"]
         else:
             raise ValueError
 
@@ -102,17 +123,30 @@ class MedMNIST(Dataset):
     def download(self):
         try:
             from torchvision.datasets.utils import download_url
-            download_url(url=self.info["url"],
-                         root=self.root,
-                         filename="{}.npz".format(self.flag),
-                         md5=self.info["MD5"])
+
+            download_url(
+                url=self.info[f"url{self.size_flag}"],
+                root=self.root,
+                filename=f"{self.flag}{self.size_flag}.npz",
+                md5=self.info[f"MD5{self.size_flag}"],
+            )
         except:
             raise RuntimeError('Something went wrong when downloading! ' +
                                'Go to the homepage to download manually. ' +
                                HOMEPAGE)
 
+    @staticmethod
+    def _collate_fn(data):
+        xs = []
+        ys = []
+        for x, y in data:
+            xs.append(np.array(x))
+            ys.append(y)
+        return np.array(xs), np.array(ys)
+
 
 class MedMNIST2D(MedMNIST):
+    available_sizes = [28, 64, 128, 224]
 
     def __getitem__(self, index):
         '''
@@ -138,12 +172,14 @@ class MedMNIST2D(MedMNIST):
 
         from medmnist.utils import save2d
 
-        save2d(imgs=self.imgs,
-               labels=self.labels,
-               img_folder=os.path.join(folder, self.flag),
-               split=self.split,
-               postfix=postfix,
-               csv_path=os.path.join(folder, f"{self.flag}.csv") if write_csv else None)
+        save2d(
+            imgs=self.imgs,
+            labels=self.labels,
+            img_folder=os.path.join(folder, f"{self.flag}{self.size_flag}"),
+            split=self.split,
+            postfix=postfix,
+            csv_path=os.path.join(folder, f"{self.flag}{self.size_flag}.csv") if write_csv else None
+        )
 
     def montage(self, length=20, replace=False, save_folder=None):
         from medmnist.utils import montage2d
@@ -159,12 +195,13 @@ class MedMNIST2D(MedMNIST):
             if not os.path.exists(save_folder):
                 os.makedirs(save_folder)
             montage_img.save(os.path.join(save_folder,
-                                          f"{self.flag}_{self.split}_montage.jpg"))
+                                          f"{self.flag}{self.size_flag}_{self.split}_montage.jpg"))
 
         return montage_img
 
 
 class MedMNIST3D(MedMNIST):
+    available_sizes = [28, 64]
 
     def __getitem__(self, index):
         '''
@@ -189,17 +226,20 @@ class MedMNIST3D(MedMNIST):
 
         assert postfix == "gif"
 
-        save3d(imgs=self.imgs,
-               labels=self.labels,
-               img_folder=os.path.join(folder, self.flag),
-               split=self.split,
-               postfix=postfix,
-               csv_path=os.path.join(folder, f"{self.flag}.csv") if write_csv else None)
+        save3d(
+            imgs=self.imgs,
+            labels=self.labels,
+            img_folder=os.path.join(folder, f"{self.flag}{self.size_flag}"),
+            split=self.split,
+            postfix=postfix,
+            csv_path=os.path.join(folder, f"{self.flag}{self.size_flag}.csv") if write_csv else None
+        )
 
     def montage(self, length=20, replace=False, save_folder=None):
         assert self.info['n_channels'] == 1
 
         from medmnist.utils import montage3d, save_frames_as_gif
+
         n_sel = length * length
         sel = np.random.choice(self.__len__(), size=n_sel, replace=replace)
 
@@ -213,7 +253,7 @@ class MedMNIST3D(MedMNIST):
 
             save_frames_as_gif(montage_frames,
                                os.path.join(save_folder,
-                                            f"{self.flag}_{self.split}_montage.gif"))
+                                            f"{self.flag}{self.size_flag}_{self.split}_montage.gif"))
 
         return montage_frames
 
@@ -294,6 +334,32 @@ class SynapseMNIST3D(MedMNIST3D):
 OrganMNISTAxial = OrganAMNIST
 OrganMNISTCoronal = OrganCMNIST
 OrganMNISTSagittal = OrganSMNIST
+
+
+def get_loader(dataset, batch_size):
+    total_size = len(dataset)
+    print('Size', total_size)
+    index_generator = shuffle_iterator(range(total_size))
+    while True:
+        data = []
+        for _ in range(batch_size):
+            idx = next(index_generator)
+            data.append(dataset[idx])
+        yield dataset._collate_fn(data)
+
+
+def shuffle_iterator(iterator):
+    # iterator should have limited size
+    index = list(iterator)
+    total_size = len(index)
+    i = 0
+    random.shuffle(index)
+    while True:
+        yield index[i]
+        i += 1
+        if i >= total_size:
+            i = 0
+            random.shuffle(index)
 
 # Function to name the datasets
 def dataset_namer(input_name, suffix):
